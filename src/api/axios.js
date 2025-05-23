2cad00db38d42053f4457cb3b47ff283ec28f539
AnnaT222 <annatazaian@gmail.com>
2025-05-24 01:14:17 +0400

    first page

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,40 +1,12 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "http://127.0.0.1:8000", // change if needed
-//   headers: {
-//     Accept: "application/json",
-//   },
-// });
-
-// // attach the JWT automatically to every request
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("accessToken");
-//   console.log("Token:", token); // Debugging line to check the token value
-
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default api;
-
-
-
-
-// src/api/axios.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000", // make sure this matches your Django runserver
+  baseURL: "http://127.0.0.1:8000", 
   headers: {
     Accept: "application/json",
-    // Don't add Authorization here — we add it dynamically below
   },
 });
 
-// Automatically attach JWT to every request
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("accessToken");
--- src/pages/BrilleBoard/BrilleBoardIntro.js
+++ src/pages/BrilleBoard/BrilleBoardIntro.js
@@ -3,21 +3,21 @@ import BrailleBoardImage from "./images/braille.png";
 
 function BrilleBoardIntro() {
   return (
-    <div>
-      <div className="brille-board">
-        <img src={BrailleBoardImage} alt="Braille Board" style={{backgroundColor:"#131313"}} />{" "}
-        <div className="brille-board-text">
-          <h1>Brille Board</h1>
-          <span>Is your dream to be fast?</span>
-          <p>
-            BB is Brille keyboard that resembles a power bank with MagSafe
-            functionality. It simply attaches to the back of your smartphone,
-            enabling you to type in any language and navigate quickly.
-          </p>
-        </div>
+    <div class="brille-board">
+      <div class="brille-board-image">
+        <img src={BrailleBoardImage} alt="Braille Board" />
       </div>
-      <div className="brille-board-button">
-        <button>Take it!</button>
+      <div class="brille-board-text">
+        <h1>Brille Board</h1>
+        <span>Is your dream to be fast?</span>
+        <p>
+          BB is a Braille keyboard that resembles a power bank with MagSafe
+          functionality. It simply attaches to the back of your smartphone,
+          enabling you to type in any language and navigate quickly.
+        </p>
+        <div class="brille-board-button">
+          <button>Take it</button>
+        </div>
       </div>
     </div>
   );
